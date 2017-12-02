689c19a0483e7a7e2e1090420d0291768067cbf6
James Ioppolo <james.ioppolo@gmail.com>
2017-12-02 11:52:21 +0800

    Added filtering of string arrays using lodash

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 import { SonnetService } from 'app/Services/SonnetService';
 import { FormsModule }   from '@angular/forms';
 
+import { SonnetModel } from 'app/models/SonnetModel';
+
+import * as _ from 'lodash';    
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
@@ -10,6 +14,7 @@ export class AppComponent {
   
   title = "Shakespeare's Sonnets";
   searchTerm: string;
+  filteredSonnets = new Array<SonnetModel>();
 
   constructor(private sonnetService: SonnetService)
   {
@@ -17,7 +22,9 @@ export class AppComponent {
 
   performSearch(searchString: string)
   {
-    console.log(`searchString=${searchString}`);
+    this.filteredSonnets = _.filter(this.sonnetService.sonnets, function(sonnet) {
+       return _.some(sonnet.lines, _.method('includes', searchString));
+    });
   }
 
 }
