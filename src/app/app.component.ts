77070f71d8fefcf228dfd457e6a2c12bdfde747c
James Ioppolo <james.ioppolo@gmail.com>
2017-12-02 15:41:21 +0800

    cleaned up unused code in the main app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,9 @@
 import { Component } from '@angular/core';
-import { SonnetService } from 'app/Services/SonnetService';
-import { FormsModule }   from '@angular/forms';
-
-import { SonnetModel } from 'app/models/SonnetModel';
-
-import * as _ from 'lodash';    
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styles: [`.title-top-space { padding-top:20px }
-    .fa-times-right-adjustment { padding-right:5px }`]
+    styles: [`.title-top-space { padding-top:20px }`]
 })
 export class AppComponent {
     title = "Shakespeare's Sonnets";
