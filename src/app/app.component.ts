30143cce5c918b94397b8ea7d980d8473665d8d7
James Ioppolo <james.ioppolo@gmail.com>
2017-12-02 12:20:21 +0800

    Cleaned up tabs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,25 +7,26 @@ import { SonnetModel } from 'app/models/SonnetModel';
 import * as _ from 'lodash';    
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html'
+    selector: 'app-root',
+    templateUrl: './app.component.html'
 })
 export class AppComponent {
-  
-  title = "Shakespeare's Sonnets";
-  searchTerm: string = '';
-  filteredSonnets = new Array<SonnetModel>();
+    title = "Shakespeare's Sonnets";
+    searchTerm: string = '';
+    filteredSonnets = new Array<SonnetModel>();
 
-  constructor(private sonnetService: SonnetService)
-  {
-  }
+    constructor(private sonnetService: SonnetService)
+    {
+    }
 
-  performSearch(searchString: string)
-  {
-    this.searchTerm = searchString;
-    this.filteredSonnets = _.filter(this.sonnetService.sonnets, function(sonnet) {
-       return _.some(sonnet.lines, _.method('includes', searchString));
-    });
-  }
-
+    performSearch(searchString: string)
+    {
+        this.searchTerm = searchString;
+        if (searchString !== '')
+        {
+            this.filteredSonnets = _.filter(this.sonnetService.sonnets, function(sonnet) {
+                return _.some(sonnet.lines, _.method('includes', searchString));
+            });
+        } 
+    }
 }
